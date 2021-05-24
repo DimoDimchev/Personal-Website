fdde986e8dcefea7b6e5f11d0443e53e130e0bd6
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-24 13:48:53 +0300

    Added About view

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from main.views import IndexView, ProjectsView
+from main.views import IndexView, ProjectsView, AboutView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('projects/', ProjectsView.as_view(), name='projects')
+    path('projects/', ProjectsView.as_view(), name='projects'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
