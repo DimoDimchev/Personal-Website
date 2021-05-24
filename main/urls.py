78a2fa246018906c0cea3594ec12c9b7e0c47163
Dimo Dimchev <ddimchev7@gmail.com>
2021-05-24 13:39:39 +0300

    Created view and template for projects

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from main.views import IndexView
+from main.views import IndexView, ProjectsView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('projects/', ProjectsView.as_view(), name='projects')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from main.models import Project
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+
+class ProjectsView(ListView):
+    model = Project
+    template_name = 'projects.html'
